4dc9297990d4319f17af603e4d17519832184ace
Stefan Kroon <steefkroon@gmail.com>
2021-02-22 16:27:28 +0100

    Finished

--- test/test_trip_service.py
+++ test/test_trip_service.py
@@ -13,12 +13,12 @@ class TestGetTripsByUser(unittest.TestCase):
     ANOTHER_USER = User()
 
     def test_raise_exception_if_user_not_logged_in(self):
-        trip_service = TestableTripService.with_user_being(self.GUEST_USER)
+        trip_service = TestableTripService().with_user_being(self.GUEST_USER).with_second_user_being(self.A_USER)
         with self.assertRaises(UserNotLoggedInException):
             trip_service.get_trips_by_user(self.A_USER)
 
     def test_no_trips_returned_if_users_are_not_friends(self):
-        trip_service = TestableTripService.with_user_being(self.A_USER)
+        trip_service = TestableTripService().with_user_being(self.A_USER)
         result = trip_service.get_trips_by_user(self.ANOTHER_USER)
         self.assertListEqual(result, [])
 
@@ -27,7 +27,7 @@ class TestGetTripsByUser(unittest.TestCase):
         friend_one = User()
         friend_one.add_friend(self.A_USER)
         friend_one.add_trip(Trip())
-        trip_service = TestableTripService.with_user_being(self.A_USER)
+        trip_service = TestableTripService().with_user_being(self.A_USER)
         result = trip_service.get_trips_by_user(friend_one)
 
         self.assertEquals(len(result), 1)
@@ -41,9 +41,10 @@ class TestableTripService(TripService):
     def _get_trips_of_user(self, user):
         return user.trips
 
-    @classmethod
-    def with_user_being(cls, user):
-        trip_service = TestableTripService()
-        trip_service.logged_in_user = user
-        return trip_service
+    def with_user_being(self, user):
+        self.logged_in_user = user
+        return self
 
+    def with_second_user_being(self, user):
+        self.logged_in_user = user
+        return self
