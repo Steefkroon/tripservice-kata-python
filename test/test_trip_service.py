29987e90e49e65bc74121d52b12b7783659eaf66
Stefan Kroon <steefkroon@gmail.com>
2021-03-08 13:15:12 +0100

    Finished with mocking instead of subclassing

--- test/test_trip_service.py
+++ test/test_trip_service.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+from unittest import mock
 
 from app.trip_service import TripService
 from exceptions import UserNotLoggedInException
@@ -12,34 +13,27 @@ class TestGetTripsByUser(unittest.TestCase):
     A_USER = User()
     ANOTHER_USER = User()
 
-    def test_raise_exception_if_user_not_logged_in(self):
-        trip_service = TestableTripService().with_user_being(self.GUEST_USER)
+    @mock.patch.object(TripService, '_get_logged_user_from_session', side_effect=UserNotLoggedInException,  autospec=True)
+    def test_raise_exception_if_user_not_logged_in(self, mock_get_user):
+        trip_service = TripService()
         with self.assertRaises(UserNotLoggedInException):
             trip_service.get_trips_by_user(self.A_USER)
+        mock_get_user.assert_called_once_with(trip_service)
 
-    def test_no_trips_returned_if_users_are_not_friends(self):
-        trip_service = TestableTripService().with_user_being(self.A_USER)
+    @mock.patch.object(TripService, '_get_logged_user_from_session', return_value=A_USER,  autospec=True)
+    def test_no_trips_returned_if_users_are_not_friends(self, mock_get_user):
+        trip_service = TripService()
         result = trip_service.get_trips_by_user(self.ANOTHER_USER)
         self.assertListEqual(result, [])
+        mock_get_user.assert_called_once_with(trip_service)
 
-    def test_return_trips_if_users_are_friends(self):
+    @mock.patch.object(TripService, '_get_trips_of_user', return_value=[Trip()], autospec=True)
+    @mock.patch.object(TripService, '_get_logged_user_from_session', return_value=A_USER,  autospec=True)
+    def test_return_trips_if_users_are_friends(self, mock_get_user, mock_get_trips_from_user):
         friend_one = User()
         friend_one.add_friend(self.A_USER)
         friend_one.add_trip(Trip())
-        trip_service = TestableTripService().with_user_being(self.A_USER)
+        trip_service = TripService()
         result = trip_service.get_trips_by_user(friend_one)
 
         self.assertEquals(len(result), 1)
-
-
-class TestableTripService(TripService):
-
-    def _get_logged_user_from_session(self):
-        return self.logged_in_user
-
-    def _get_trips_of_user(self, user):
-        return user.trips
-
-    def with_user_being(self, user):
-        self.logged_in_user = user
-        return self
